03dc9177d9d35e26be800ad34cf43eead7205928
Amit Kumar Dhurway <125995584+AmitDhurway@users.noreply.github.com>
2024-05-13 15:06:08 +0530

    update resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ export default function Home() {
                             <p className='text-sm md:text-md text-justify'>I'm a MERN stack developer adept at building dynamic web applications, with expertise in MongoDB, Express.js, React.js, and Node.js. I thrive on solving complex challenges, staying updated with the latest technologies, and collaborating effectively to deliver innovative solutions that enhance user experiences.</p>
                         </div>
                         <br />
-                        <a type="submit" className='border border-black text-sm p-1 h-7 text-white bg-orange-600 rounded-full cursor-pointer hover:scale-105 hover:text-black duration200' href="/resume.pdf" target='_blank'>Download CV!</a>
+                        <a type="submit" className='border border-black text-sm p-1 h-7 text-white bg-orange-600 rounded-full cursor-pointer hover:scale-105 hover:text-black duration200' href="/A-Resume.pdf" target='_blank'>Download CV!</a>
                         
                         <div className='flex justify-between items-center pt-8 flex-col md:flex-row space-y-5 md:space-y-0'>
                             <div className='space-y-2 text-xl'>
